039626869a4d244bde3346bd938c4c6240780357
nilkanth4077 <nilkanth4077@gmail.com>
2026-04-27 13:00:30 +0530

    Comment removed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",  // Ensures correct base path
+  base: "/",
   server: {
     historyApiFallback: true,  // Fixes 404 issues on refresh
   },
